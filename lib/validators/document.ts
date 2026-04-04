a49a778eae07bee9b7ecef3439e6e2b124a77595
Pruthviraj141 <pruthvigavhane@gmail.com>
2026-04-04 19:16:10 +0530

    yml fix

--- lib/validators/document.ts
+++ lib/validators/document.ts
@@ -3,14 +3,14 @@ import { z } from "zod";
 export const documentIdSchema = z.string().uuid();
 
 export const createDocumentSchema = z.object({
-  title: z.string().trim().min(1).max(180),
+  title: z.string().max(180),
   content: z.string().default(""),
   workspaceId: z.string().uuid()
 });
 
 export const updateDocumentSchema = z.object({
   id: z.string().uuid(),
-  title: z.string().trim().min(1).max(180).optional(),
+  title: z.string().max(180).optional(),
   content: z.string().optional(),
   isArchived: z.boolean().optional()
 });
